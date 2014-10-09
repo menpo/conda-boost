38549516b5ea5d74dc23a3394fd9a919264a32c0
Patrick Snape <patrick.snape08@imperial.ac.uk>
2014-10-09 13:14:21 +0100

    Try appending 'm' to PY_VER

--- conda/build.sh
+++ conda/build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-./bootstrap.sh --with-python-version=$PY_VER --with-python-root="$PREFIX"
+if [ $PY3K ]; then
+  export M_PY_VER="$PY_VERm"
+else
+  export M_PY_VER="$PY_VER"
+fi
+
+./bootstrap.sh --with-python-version="$M_PY_VER" --with-python-root="$PREFIX"
 ./bjam -j2 -sBZIP2_LIBPATH="$PREFIX/lib" -sBZIP2_INCLUDE="$PREFIX/include" link=shared stage
 
 mkdir -p $PREFIX/lib
