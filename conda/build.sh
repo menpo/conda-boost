eaa24cc2e73ea776575d503c201d7f67178b3720
Patrick Snape <patricksnape@gmail.com>
2014-10-09 16:14:15 +0100

    Try working out the include path

--- conda/build.sh
+++ conda/build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# The bootstrap.sh is broken for Python3
 sed -i 's:using python:#using python:g' bootstrap.sh
+
+# Grab the include dir (borrowed from homebrew boost-python)
+export PYTHON_INCLUDE_DIR=`python -c "from __future__ import print_function; import distutils.sysconfig; print(distutils.sysconfig.get_python_inc(True))"`
 
 ./bootstrap.sh 
 ./bjam --debug-configuration --user-config="$RECIPE_DIR/user-config.jam" -j2 -sBZIP2_LIBPATH="$PREFIX/lib" -sBZIP2_INCLUDE="$PREFIX/include" link=shared stage
