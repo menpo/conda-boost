5787fd1a5a78eae43fb4a3202825972720c83260
Patrick Snape <patrick.snape08@imperial.ac.uk>
2014-10-09 11:28:35 +0100

    Try using PY_VER for Python 3 on Travis

--- conda/build.sh
+++ conda/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./bootstrap.sh
+./bootstrap.sh --with-python=python$PY_VER
 ./bjam -j2 -sBZIP2_LIBPATH="$PREFIX/lib" -sBZIP2_INCLUDE="$PREFIX/include" link=shared stage
 
 mkdir -p $PREFIX/lib
