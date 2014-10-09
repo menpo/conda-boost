5c8a6352e9694562f59ed7470222c3e514780ef1
Patrick Snape <patrick.snape08@imperial.ac.uk>
2014-10-09 11:46:21 +0100

    Try explicitly setting the toolset?

--- conda/bld.bat
+++ conda/bld.bat
@@ -2,8 +2,14 @@
 
 setlocal enableextensions
 
+if %PY3K% (
+    SET MSVC_VER="10.0"
+) else (
+    SET MSVC_VER="9.0"
+)
+
 bootstrap
-bjam -j2 -sBZIP2_LIBPATH="%PREFIX%\lib" -sBZIP2_INCLUDE="%PREFIX%\include" link=shared stage
+bjam -j2 -sBZIP2_LIBPATH="%PREFIX%\lib" -sBZIP2_INCLUDE="%PREFIX%\include" link=shared toolset="msvc-%MSVC_VER%" stage
 
 md "%PREFIX%\lib"
 robocopy "stage\lib" "%PREFIX%\lib" /E
