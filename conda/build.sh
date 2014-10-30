6e144f627c3df67c88df8ea6d1d2789b6d5a02f4
Patrick Snape <patricksnape@gmail.com>
2014-10-30 11:45:53 +0000

    Bump build number and chmod whole dir

--- conda/build.sh
+++ conda/build.sh
@@ -6,7 +6,7 @@ sed -i 's:using python:#using python:g' bootstrap.sh
 # Grab the include dir (borrowed from homebrew boost-python)
 export PYTHON_INCLUDE_DIR=`python -c "from __future__ import print_function; import distutils.sysconfig; print(distutils.sysconfig.get_python_inc(True))"`
 
-chmod 777 boostrap.sh
+chmod -R 777 .*
 ./bootstrap.sh
 ./bjam --debug-configuration --user-config="$RECIPE_DIR/user-config.jam" -j2 -sBZIP2_LIBPATH="$PREFIX/lib" -sBZIP2_INCLUDE="$PREFIX/include" link=shared stage
 
